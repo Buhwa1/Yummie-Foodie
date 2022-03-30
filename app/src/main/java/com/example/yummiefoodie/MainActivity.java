278e0793a4c3cc822e169fc68cafbeed42fa8319
Buhwa1 <ezybuhwa@gmail.com>
2022-03-30 21:02:30 +0300

    First Commit

---
+++ app/src/main/java/com/example/yummiefoodie/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.yummiefoodie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import info.hoang8f.widget.FButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnsignin,btnsignup;
+    TextView txtslogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnsignin = findViewById(R.id.btnsignin);
+        btnsignup = findViewById(R.id.btnsignup);
+
+        txtslogan = findViewById(R.id.txttitle);
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
