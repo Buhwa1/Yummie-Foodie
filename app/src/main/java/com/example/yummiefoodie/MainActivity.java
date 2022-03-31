50caf435998ef3ec9c1903a729f10bccd827da1e
Buhwa1 <ezybuhwa@gmail.com>
2022-03-31 18:50:35 +0300

    Signup Commit 2

--- app/src/main/java/com/example/yummiefoodie/MainActivity.java
+++ app/src/main/java/com/example/yummiefoodie/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yummiefoodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
         btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+            }
+        });
+
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signin = new Intent(MainActivity.this,SignIn.class);
+                startActivity(signin);
 
             }
         });
